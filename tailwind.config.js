5bd4e26898cdbad44d110c407644888862b38da2
Nika M. <merabishvilinikusha@gmail.com>
2024-05-14 22:27:43 +0400

    Doneexcept js logic of the dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: "selector",
   theme: {
     screens: {
       mobile: "375px",
@@ -11,6 +12,7 @@ module.exports = {
       colors: {
         custom: {
           100: "#8C98C6",
+          200: "#63687D",
           greenText: "#1EB589",
           redText: "#DC414C",
         },
